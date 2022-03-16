391219089f4bff573a699f1ac53d35d84b60090d
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-16 23:51:19 +0000

    Green test

--- LeapYearKata/LeapYear.cs
+++ LeapYearKata/LeapYear.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +25,7 @@ namespace LeapYearKata
 
         public static bool CheckIfDateIsDivisibleBy100ButNotBy400(int input)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
